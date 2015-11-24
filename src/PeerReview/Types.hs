73397a23fb8727e54838727a5e33678a9b2c7db9
Matias Keveri <keveri@users.noreply.github.com>
2015-11-24 21:27:55 +0200

    Add database related instances.

--- src/PeerReview/Types.hs
+++ src/PeerReview/Types.hs
@@ -2,7 +2,13 @@
 module PeerReview.Types where
 
 import           Data.Aeson
-import           Data.Text      (Text)
+import           Data.Text                            (Text)
+import qualified Data.Text.Encoding                   as T
+import qualified Database.PostgreSQL.Simple           as PG
+import qualified Database.PostgreSQL.Simple.FromField as PG
+import qualified Database.PostgreSQL.Simple.FromRow   as PG
+import qualified Database.PostgreSQL.Simple.ToField   as PG
+import qualified Database.PostgreSQL.Simple.ToRow     as PG
 import           Web.Spock.Safe
 
 data AppConfig = AppConfig
@@ -79,3 +85,33 @@ instance FromJSON PeerReview where
                    <*> v .: "reviewerId"
                    <*> v .: "status"
     parseJSON _ = mempty
+
+
+instance PG.FromField ReviewStatus where
+    fromField f Nothing  = PG.returnError PG.UnexpectedNull f ""
+    fromField _ (Just b) =
+        case T.decodeUtf8 b of
+              "WAITING"  -> return Waiting
+              "REVIEWED" -> return Reviewed
+              "ACCEPTED" -> return Accepted
+              _          -> error "invalid status"
+
+instance PG.FromRow ReviewStatus where
+    fromRow = PG.field
+
+instance PG.ToField ReviewStatus where
+    toField Waiting  = PG.toField ("WAITING"  :: Text)
+    toField Reviewed = PG.toField ("REVIEWED" :: Text)
+    toField Accepted = PG.toField ("ACCEPTED" :: Text)
+
+instance PG.ToRow PeerReview where
+    toRow (PeerReview taskId comment score reviewerId status) =
+        PG.toRow (taskId, comment, score, reviewerId, status)
+
+instance PG.FromRow PeerReview where
+    fromRow =
+        PeerReview <$> PG.field
+                   <*> PG.field
+                   <*> PG.field
+                   <*> PG.field
+                   <*> PG.field
