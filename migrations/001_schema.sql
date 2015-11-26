48d00af4e9bf9752d1df932b6b8c444432920fbe
Matias Keveri <keveri@users.noreply.github.com>
2015-11-26 17:16:41 +0200

    Use 'IF EXISTS' on db creation. This way it won't fail even when same migration is ran multiple times.

--- migrations/001_schema.sql
+++ migrations/001_schema.sql
@@ -1,8 +1,20 @@
-create type STATUS as enum ('WAITING', 'REVIEWED', 'ACCEPTED');
-create table peer_reviews (
-  task_id text not null,
+-- create types
+DO $$
+BEGIN
+  IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'status') THEN
+    CREATE TYPE status AS enum (
+        'WAITING',
+        'REVIEWED',
+        'ACCEPTED'
+    );
+  END IF;
+END$$;
+
+-- create tables
+CREATE TABLE IF NOT EXISTS peer_reviews (
+  task_id text NOT NULL,
   comment text,
   score int,
-  reviewer_id text not null,
-  status STATUS not null
+  reviewer_id text NOT NULL,
+  status STATUS NOT NULL
 );
