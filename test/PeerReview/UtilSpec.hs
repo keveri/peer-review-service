7298054ffc0a7d2abcccaf9684a1c2706fc593b7
Matias Keveri <keveri@users.noreply.github.com>
2015-12-07 17:55:43 +0200

    Add test for Util module function.

---
+++ test/PeerReview/UtilSpec.hs
@@ -0,0 +1,22 @@
+{-# LANGUAGE OverloadedStrings #-}
+module PeerReview.UtilSpec
+  ( main
+  , spec
+  ) where
+
+import           Test.Hspec
+
+import           PeerReview.Types
+import           PeerReview.Util
+
+main :: IO ()
+main = hspec spec
+
+spec :: Spec
+spec =
+  describe "reviewFromSub" $
+    it "Constructs an peer review from user id and submission data" $
+      let uid         = "0"
+          submission  = Submission "1" "2" "3" Nothing
+          expectation = PeerReview "1" "3" "" 0 "0" Waiting
+      in expectation `shouldBe` reviewFromSub uid submission
